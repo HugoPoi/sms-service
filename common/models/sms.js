9585f3987c8ba183c3febb26c40545d6ec41c5ba
HugoPoi <hugo.poissonnet@gmail.com>
2015-07-06 09:39:47 +0200

    write file with message v1

--- common/models/sms.js
+++ common/models/sms.js
@@ -1,21 +1,16 @@
 var fs = require('fs');
 module.exports = function(Sms) {
   Sms.send = function(number, msg, cb) {
-    fs.open(path, 'wx', function(err, fd){
-      if(err){
-        cb(err);
-      }
-      fs.write(fd, number + '\r\n' + msg + '\r\n', function(err, state){
-
-      });
+    fs.writeFile( Sms.settings.smsPath + '/test.msg', number + '\r\n' + msg + '\r\n', { flag : 'wx'}, function(err){
+      //TODO: check if the file have been delete before callback
+          return cb(err, 'Success send "' + msg + '" to '+ number);
     });
-    cb(null, 'Success send "' + msg + '" to '+ number);
   }
   Sms.remoteMethod(
-        'send',
-        {
-          accepts: [{arg: 'number', type: 'string'},{arg: 'msg', type: 'string'}],
-          returns: {arg: 'successMsg', type: 'string'}
-        }
-    );
+    'send',
+    {
+      accepts: [{arg: 'number', type: 'string'},{arg: 'msg', type: 'string'}],
+      returns: {arg: 'successMsg', type: 'string'}
+    }
+  );
 };
